aaa46ff12a6a8362372ffb47de918324d107430e
Sophia <122832728+spnorrington@users.noreply.github.com>
2023-07-18 02:04:02 -0500

    Create Point.h

---
+++ Classes /Coordinate System Convertible Point Class/Point.h
@@ -0,0 +1,30 @@
+//-------------Header File-----------------//
+
+#ifndef POINT_H
+#define POINT_H
+
+class Point
+{
+private:
+	double x;
+	double y;
+	double z;
+
+public:
+	Point() : x(0), y(0), z(0) {}
+	Point(double newX, double newY, double newZ) : x(newX), y(newY), z(newZ) {}
+
+	void setCoordinates(double newX, double newY, double newZ);
+	void move(double offsetX, double offsetY, double offsetZ);
+	void move(Point offset);
+	double getX() const;
+	double getY() const;
+	double getZ() const;
+
+	// Declarations of Spherical Coordinate Functions
+	double radius(double x, double y, double z);
+	double theta(double x, double y);
+	double rho(double x, double y, double z);
+};
+
+#endif
